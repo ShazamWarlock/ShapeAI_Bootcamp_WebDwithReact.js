55663d763ac6e04dfb6ae712a115da00fe27e493
ShazamWarlock <74046329+ShazamWarlock@users.noreply.github.com>
2021-06-27 18:04:33 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,9 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./components/App";
+var name = "Shazam";
+ReactDOM.render(
+  <App />,
+
+  document.getElementById("root")
+);
